084a4fe1e1f434f55f76eb5c026579182acbc357
Angel Motta <angel.motta@utec.edu.pe>
2023-01-31 17:24:05 -0500

    feat: add contact implementation

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,13 +1,44 @@
 import "./AddContact.css";
 import { useContext, useState } from 'react';
-import { UserContext } from '../context/UserContext'
+import { UserContext } from '../context/UserContext';
+import { endpoints } from '../config/uri';
 
 function AddContact() {
     const [newContact, setNewContact] = useState('');
     const { userSession } = useContext(UserContext);    
 
-    const handleSubmit = async (e, contactObj) => {
+    const handleSubmit = async (e) => {
         console.log(`POST addContact request`);
+        e.preventDefault();
+        const requestNewContact = {
+            contact: newContact,
+        }
+        // Send Post Request
+        let response;
+        try {
+            response = await fetch(endpoints.addContact, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: userSession.token,
+                },
+                body: JSON.stringify(requestNewContact),
+            });
+        } catch (e) {
+            console.log(`Catch Request Error: Servicio no disponible`);
+            console.log(e);
+        }
+
+        let dataResp;
+        if (response?.ok) {
+            dataResp = await response.json();
+            setNewContact('');
+            // TODO: Update userSession Object with new contact
+        } else {
+            console.log(`Status not 200 series`);
+            console.log(`HTTP Status code: ${response?.status}`);
+        }
     }
 
     return(
--- src/config/uri.js
+++ src/config/uri.js
@@ -2,4 +2,5 @@ export const endpoints = {
     login: 'http://localhost:4000/login',
     chat: 'http://localhost:4000/chat',
     sendMessage: 'http://localhost:4000/message',
+    addContact: 'http://localhost:4000/contact',
 };
